cc971146c5e4567b8057e150c119228daa2437e9
jpristax <122694601+Pristax@users.noreply.github.com>
2026-06-18 12:34:19 +0200

    0.0.1

---
+++ README.md
@@ -0,0 +1,2 @@
+# Voxel-Engine
+A voxel engine built in Python using Pygame. It demonstrates basic 3D-style voxel rendering, world generation, block interaction, and real-time movement in a lightweight game-engine prototype. Useful as an educational project for learning graphics, game loops, and voxel-based environments in Python.
---
+++ settings.py
@@ -0,0 +1,23 @@
+from numba import njit
+import numpy as np
+import glm
+import math
+
+WIN_RESOLUTION = glm.vec2(1600, 900)
+
+# camera
+ASPECT_RATIO = WIN_RESOLUTION.x / WIN_RESOLUTION.y
+FOG_DEG = 50
+V_FOV = glm.radians(FOV_DEG) # vertical fov
+H_FOV = 2 * math.atan(math.tan(V_FOV * 0.5) * ASPECT_RATIO) # horizontal fov
+NEAR = 0.1
+FAR = 2000.0
+PITCH_MAX = glm.radians(89)
+
+# player
+PLAYER_SPEED = 0.005
+PLAYER_ROT_SPEED = 0.003
+PLAYER_POS = glm.vec3(0, 0, 1)
+MOUSE_SENSITIVITY = 0.002
+
+BG_COLOR = glm.vec3(0.1, 0.16, 0.25)
